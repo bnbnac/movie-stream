63b335fd55ec67e7297f6080f04e7e814588e94a
bnbnac <bnbnac@naver.com>
2023-09-08 21:30:59 +0900

    init

---
+++ src/main/java/com/bnbnac/moviestream/MovieStreamApplication.java
@@ -0,0 +1,13 @@
+package com.bnbnac.moviestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnbnac/moviestream/MovieStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnbnac.moviestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
