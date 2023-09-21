1a5e4f603e235e79ef5441c4a0fdcf2d4e907f15
bnbnac <bnbnac@naver.com>
2023-09-21 21:44:35 +0900

    add utility

--- src/main/java/com/bnbnac/moviestream/service/PasswordService.java
+++ src/main/java/com/bnbnac/moviestream/service/PasswordService.java
@@ -1,15 +1,19 @@
 package com.bnbnac.moviestream.service;
 
+import com.bnbnac.moviestream.constant.EnvironmentVariable;
 import com.bnbnac.moviestream.exception.LoadPasswordException;
+import com.bnbnac.moviestream.utils.Utility;
 import org.springframework.stereotype.Service;
+
+import static com.bnbnac.moviestream.utils.Utility.isNullOrBlank;
 
 @Service
 public class PasswordService {
     private final String password;
 
     public PasswordService() {
-        this.password = System.getenv("MOVIESTREAM");
-        if (password == null) {
+        password = System.getenv(EnvironmentVariable.MOVIE_STREAM);
+        if (isNullOrBlank(password)) {
             throw new LoadPasswordException();
         }
     }
---
+++ src/main/java/com/bnbnac/moviestream/utils/Utility.java
@@ -0,0 +1,7 @@
+package com.bnbnac.moviestream.utils;
+
+public class Utility {
+    public static boolean isNullOrBlank(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+}
